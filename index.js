be8237af966f93fb9efb08de8a827463f7566ea4
dy <df.creative@gmail.com>
2018-12-05 07:51:11 -0500

    Extend the API

--- index.js
+++ index.js
@@ -6,11 +6,16 @@
 
 module.exports = {
 	saw: require('./saw'),
+	sawtooth: require('./saw'),
 	series: require('./series'),
 	sin: require('./sin'),
+	sine: require('./sin'),
 	cos: require('./cos'),
+	cosine: require('./cos'),
 	tri: require('./tri'),
+	triangle: require('./tri'),
 	rect: require('./rect'),
+	square: require('./rect'),
 	clausen: require('./clausen'),
 	step: require('./step')
 }
